9c09931fcbd19593567ae42e286a588203e5d69e
Somprasong Damyos <somprasong.damyos@gmail.com>
2022-04-20 16:21:01 +0700

    add business logic

--- goapi/go.mod
+++ goapi/go.mod
@@ -3,7 +3,16 @@ module goapi
 go 1.17
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/go-playground/locales v0.14.0
+	github.com/go-playground/universal-translator v0.18.0
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/go-playground/validator/v10 v10.10.1
+	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/postgres v1.3.4
+	gorm.io/gorm v1.23.4
+)
+
+require (
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.11.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -14,8 +23,9 @@ require (
 	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gorm.io/driver/postgres v1.3.4 // indirect
-	gorm.io/gorm v1.23.4 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- goapi/pkg/handlers/todo.go
+++ goapi/pkg/handlers/todo.go
@@ -2,44 +2,138 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
+	"goapi/pkg/common/database"
+	"goapi/pkg/common/validator"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
 type Todo struct {
 	ID        int    `json:"id"`
-	Title     string `json:"text"`
-	Completed bool   `json:"completed" gorm:"column:is_done"`
+	Text      string `json:"text" gorm:"column:title" validate:"required" `
+	Completed bool   `json:"isCompleted" gorm:"column:is_done"`
 }
 
 func CreateTodo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Create Todo")
+	// step 1: แปลง JSON จาก request body เป็น Todo struct
+	var todo Todo
+
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// step 2: validate
+	err = validator.ValidateStruct(todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// step 3: insert
+	tx := database.DB.Create(&todo)
+	if err := tx.Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// step 4: response
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(todo)
 }
 
 func ListTodo(w http.ResponseWriter, r *http.Request) {
-	todos := []Todo{
-		{ID: 1, Title: "Test 1", Completed: true},
-		{ID: 2, Title: "Test 2", Completed: false},
-		{ID: 3, Title: "Test 3", Completed: false},
+	// เพิ่มอ่านค่าจาก query params
+	query := r.URL.Query()
+	wheres := map[string]interface{}{}
+	// ถ้าส่ง completed มา ให้ใส่ไปใน map["is_done"] ตามชื่อ column จริงในฐานข้อมูล
+	if val, ok := query["completed"]; ok {
+		b1, err := strconv.ParseBool(val[0])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		wheres["is_done"] = b1
 	}
-	// ส่งข้อมูลทั้งหมดกลับไปในรูปแบบ JSON
+
+	todos := []Todo{}
+	// เพิ่ม .Where()
+	tx := database.DB.Where(wheres).Find(&todos)
+
+	if err := tx.Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(todos)
 }
 
 func GetTodo(w http.ResponseWriter, r *http.Request) {
+	// step 1: get id from path param
 	vars := mux.Vars(r)
-	fmt.Fprint(w, "Get Todo by ID:", vars["id"])
+	id, _ := strconv.Atoi(vars["id"])
+	// step 2: select where id
+	todo := Todo{}
+	tx := database.DB.First(&todo, id)
+	if err := tx.Error; err != nil {
+		// step 3: handle error not found
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(w, "todo with given id not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// step 4: response
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
 }
 
 func UpdateTodoStatus(w http.ResponseWriter, r *http.Request) {
+	// step 1: get id from path param
 	vars := mux.Vars(r)
-	fmt.Fprint(w, "Update Todo Status by ID:", vars["id"])
+	id, _ := strconv.Atoi(vars["id"])
+	// step 2: แปลง json body เป็น struct เพื่อเอาค่าสถานะที่ส่งมา
+	var todo Todo
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// step 3: update only is_done column
+	tx := database.DB.Model(Todo{ID: id}).Update("is_done", todo.Completed)
+	if err := tx.Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// step 4: handle not found error
+	if tx.RowsAffected == 0 {
+		http.Error(w, "todo with given id not found", http.StatusNotFound)
+		return
+	}
+	// step 5: response
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	// step 1: get id from path param
 	vars := mux.Vars(r)
-	fmt.Fprint(w, "Delete Todo by ID:", vars["id"])
+	id, _ := strconv.Atoi(vars["id"])
+	// step 2: delete where id
+	tx := database.DB.Delete(&Todo{}, id)
+	if err := tx.Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// step 3: handle not found error
+	if tx.RowsAffected <= 0 {
+		http.Error(w, "todo with given id not found", http.StatusNotFound)
+		return
+	}
+	// step 4: response
+	w.WriteHeader(http.StatusNoContent)
 }
