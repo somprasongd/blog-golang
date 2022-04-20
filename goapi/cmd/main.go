56ec5c2d8da727fe0927d94e58b9ecf1378318e9
Somprasong Damyos <somprasong.damyos@gmail.com>
2022-04-20 15:22:12 +0700

    URL Parameters

--- goapi/cmd/main.go
+++ goapi/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -17,6 +18,11 @@ func main() {
 	r := mux.NewRouter()
 	// define route
 	r.HandleFunc("/tests", handleTest)
+	// สามารถใช้ร่วมกับ regx ได้
+	r.HandleFunc("/tests/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Fprint(w, vars["id"])
+	})
 
 	// starting server
 	log.Fatal(http.ListenAndServe(":8080", r))
