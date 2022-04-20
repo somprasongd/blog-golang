4833a3e91b5179f692dec2160aac53e51bf91198
Somprasong Damyos <somprasong.damyos@gmail.com>
2022-04-20 15:03:06 +0700

    Request and Response Headers

--- goapi/cmd/main.go
+++ goapi/cmd/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// define route
+	http.HandleFunc("/greet", greet)
+
 	// starting server
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
 }
